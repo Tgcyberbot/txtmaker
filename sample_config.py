fe7642e26d325d945a5b52ddb22361996ff02c4c
Tgcyberbot <141454564+Tgcyberbot@users.noreply.github.com>
2023-08-06 00:29:03 +0530

    config.py

--- sample_config.py
+++ sample_config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config(object):
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-    APP_ID = int(os.environ.get("APP_ID", 12345))
-    API_HASH = os.environ.get("API_HASH")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6664755882:AAG0qJHptIXS4YaB6-n-8B7cejl_thmv800")
+    APP_ID = int(os.environ.get("APP_ID", 24131567))
+    API_HASH = os.environ.get("API_HASH", "7bf12fc2ec81ba350bca34c014549bee")
     DOWNLOAD_LOCATION = "./DOWNLOADS"
